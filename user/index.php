09c167fc4243d49c1ea36e41675b96f71d1962da
matthieugras63 <matthieu.gras.63@gmail.com>
2019-10-15 13:57:42 +0200

    preview of index layout

--- user/index.php
+++ user/index.php
@@ -1,18 +1,30 @@
-<?php require_once "../connection.php" ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <title>Document</title>
-  <link rel="stylesheet" type="text/css" href="style/style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <form action="forms/register.php" method="POST">
-    <input type="submit" name="register" value="S'inscrire">
-  </form>
-  <form action="forms/connect.php" method="POST">
-    <input type="submit" name="connect" value="Se connecter">
-  </form>
+
+  <header>
+    <nav>
+      <ul>
+        <li>Accueil</li>
+        <li>Inscription</li>
+        <li>Connexion</li>
+      </ul>
+    </nav>
+    <h1>Bienvenue sur la boutique Chaustore</h1>
+  </header>
+
+  <main>
+
+
+
+  </main>
+
+  <script type="text/javascript" src="../backoffice/scripts/jquery-3.4.1.js"></script>
+  <script type="text/javascript" src="../backoffice/scripts/script.js"></script>
 </body>
 </html>
