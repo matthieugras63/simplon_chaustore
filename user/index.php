1f174c3f15e45bb293a9f7b942c13ac0b3a1076f
matthieugras63 <matthieu.gras.63@gmail.com>
2019-10-18 14:08:45 +0200

    products now displayed with or without images

--- user/index.php
+++ user/index.php
@@ -1,4 +1,5 @@
 <?php session_start()  ?>
+<?php require_once '../connection.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,17 +17,52 @@
         <?php if (!isset($_SESSION['id'])): ?>
           <li>Inscription</li>
           <li>Connexion</li>
-        <?php else : ?>
-          <li>Panier</li>
-          <li>Déconnexion</li>
-        <?php endif ?>
-      </ul>
-    </nav>
-    <h1>Bienvenue sur la boutique Chaustore <?php if (isset($_SESSION['id'])) {echo" , ". $_SESSION['firstname'];} ?></h1>
-  </header>
+          <?php else : ?>
+            <li>Panier</li>
+            <li>Déconnexion</li>
+          <?php endif ?>
+        </ul>
+      </nav>
+      <h1>Bienvenue sur la boutique Chaustore <?php if (isset($_SESSION['id'])) {echo" , ". $_SESSION['firstname'];} ?></h1>
+    </header>
 
-  <main>
+    <main>
 
+      <?php
+
+      $products = "SELECT product.name AS product, category.name AS category, brand.name AS brand ,price, image ,sum(stock) as stock FROM stock INNER JOIN product ON stock.product_id = product.id INNER JOIN brand ON product.brand_id = brand.id INNER JOIN category ON product.category_id = category.id GROUP BY product_id ORDER BY product.name;";
+
+      $req = mysqli_query($conn, $products);
+      while($result = mysqli_fetch_array($req)){
+        for ($i=0; $i<count($result); $i++)
+         ?>
+       <div>
+        <?php
+        $msg ="";
+        ?>
+        <?php if ($result['image'] !== ''): ?>
+         <img src="../backoffice/form_add/images/<?php echo $result['image'] ?>" alt="<?php echo $result['product'] ?>" />
+         <?php else : ?>
+          <p class="noImage">Aucune image disponible pour le moment</p>
+         <?php endif ;?>
+         <p><span>Produit</span> : <?php echo $result['product'] ?> </p>
+         <p><span>Marque</span> : <?php echo $result['brand'] ?> </p>
+         <p><span>Catégorie</span> : <?php echo$result['category'] ?> </p>
+         <p><span>Prix</span> : <?php echo $result['price'] ?> € </p>
+         <?php
+         if ($result['stock'] == 0) {
+          echo "<font color='red'>Aucun produit en stock</font>";
+        } else if ($result['stock'] > 0 && $result['stock'] <= 10 ){
+          echo "<font color = 'orange'> Produit presque épuisé</font>";
+        } else {
+          echo "<font color = 'green'> Produit disponible</font>";
+        }
+        ?>
+      </div>
+      <?php
+    }
+
+    ?>
 
 
   </main>
